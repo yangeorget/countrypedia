72bf54350e0b1e48cee8f945fa0af9d3ca588a7f
Yan Georget <yan.georget@polytechnique.org>
2015-09-10 23:13:06 +0200

    using nokogiri

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,8 @@
+require 'nokogiri'
+require 'open-uri'
+
 class Country < ActiveRecord::Base
+  def wikipedia
+    doc = Nokogiri::HTML(open("https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal"))
+  end
 end
