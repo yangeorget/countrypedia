f1f8e3d9dc4b3198c9b914a445abd0128478e1b5
Yan Georget <yan.georget@polytechnique.org>
2015-09-22 14:22:01 +0200

    about

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   match 'countries/random', to: 'countries#random', via: [:get]
   resources :countries, :only => [:index, :show]
+  get "/pages/:page" => "pages#show"
   root to: redirect('/countries')
 end
